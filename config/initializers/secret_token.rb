e5abb81ea8cbc7819f44c71f9de62b22d9363822
Gearoid Garvey <gearoid.garvey@gmail.com>
2011-08-06 15:31:27 +0100

    predictoball_v3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PredictoballV3::Application.config.secret_token = 'bd83741afafae26c795cb6393712e89ae79b58aafc3bd389589abe6b5c2231a1e79ef2f1f93f2fb9793c6e039c0a6be57346e90078f2943c88ead1df1402bf2c'
